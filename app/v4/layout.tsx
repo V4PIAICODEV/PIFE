422b8dd2cb09d4f0f67cc6e1df51fdf9764df2eb
V4PIAICODEV <157130484+V4PIAICODEV@users.noreply.github.com>
2026-02-02 14:48:17 -0300

    Update layout.tsx

--- app/v4/layout.tsx
+++ app/v4/layout.tsx
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 import authOptions from "@/lib/auth";
-import { LayoutClient } from "./layout.client";
+import { LayoutClient } from "./layoutclient";
 import { redirect } from "next/navigation";
 
 export default async function LayoutV4({ children }: { children: React.ReactNode }) {
